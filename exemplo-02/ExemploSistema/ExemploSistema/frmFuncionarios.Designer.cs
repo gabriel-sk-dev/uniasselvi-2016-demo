7806a009a604e9e5a29c422d23c3beb6addbb741
gskohlrausch <gabriel@society.com.br>
2016-12-16 21:56:43 -0200

    exemplo 02 - added dapper

---
+++ exemplo-02/ExemploSistema/ExemploSistema/frmFuncionarios.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ExemploSistema
+{
+    partial class frmFuncionarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabList = new System.Windows.Forms.TabPage();
+            this.tabDetails = new System.Windows.Forms.TabPage();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cboCivilState = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.tabList.SuspendLayout();
+            this.tabDetails.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabList);
+            this.tabControl.Controls.Add(this.tabDetails);
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(649, 288);
+            this.tabControl.TabIndex = 1;
+            this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+            // 
+            // tabList
+            // 
+            this.tabList.Controls.Add(this.btnDelete);
+            this.tabList.Controls.Add(this.btnEdit);
+            this.tabList.Controls.Add(this.btnNew);
+            this.tabList.Controls.Add(this.dataGrid);
+            this.tabList.Location = new System.Drawing.Point(4, 22);
+            this.tabList.Name = "tabList";
+            this.tabList.Padding = new System.Windows.Forms.Padding(3);
+            this.tabList.Size = new System.Drawing.Size(641, 262);
+            this.tabList.TabIndex = 0;
+            this.tabList.Text = "Lista";
+            this.tabList.UseVisualStyleBackColor = true;
+            // 
+            // tabDetails
+            // 
+            this.tabDetails.Controls.Add(this.label3);
+            this.tabDetails.Controls.Add(this.txtSalary);
+            this.tabDetails.Controls.Add(this.label2);
+            this.tabDetails.Controls.Add(this.cboCivilState);
+            this.tabDetails.Controls.Add(this.txtName);
+            this.tabDetails.Controls.Add(this.label1);
+            this.tabDetails.Controls.Add(this.btnCancel);
+            this.tabDetails.Controls.Add(this.btnSave);
+            this.tabDetails.Location = new System.Drawing.Point(4, 22);
+            this.tabDetails.Name = "tabDetails";
+            this.tabDetails.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDetails.Size = new System.Drawing.Size(641, 262);
+            this.tabDetails.TabIndex = 1;
+            this.tabDetails.Text = "Detalhes";
+            this.tabDetails.UseVisualStyleBackColor = true;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(6, 6);
+            this.dataGrid.MultiSelect = false;
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid.Size = new System.Drawing.Size(629, 221);
+            this.dataGrid.TabIndex = 1;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(6, 233);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "Novo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(87, 233);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Alterar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(168, 233);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(560, 233);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(479, 233);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nome";
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(116, 15);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(311, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // cboCivilState
+            // 
+            this.cboCivilState.Enabled = false;
+            this.cboCivilState.FormattingEnabled = true;
+            this.cboCivilState.Items.AddRange(new object[] {
+            "Casado",
+            "Solteiro",
+            "Viuvo",
+            "Divorciado"});
+            this.cboCivilState.Location = new System.Drawing.Point(116, 41);
+            this.cboCivilState.Name = "cboCivilState";
+            this.cboCivilState.Size = new System.Drawing.Size(121, 21);
+            this.cboCivilState.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Estado civil";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Enabled = false;
+            this.txtSalary.Location = new System.Drawing.Point(116, 68);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(121, 20);
+            this.txtSalary.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Salário";
+            // 
+            // frmFuncionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 312);
+            this.Controls.Add(this.tabControl);
+            this.Name = "frmFuncionarios";
+            this.Text = "Manutenção de funcionários";
+            this.Load += new System.EventHandler(this.frmFuncionarios_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabList.ResumeLayout(false);
+            this.tabDetails.ResumeLayout(false);
+            this.tabDetails.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabList;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.TabPage tabDetails;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboCivilState;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ exemplo-02/ExemploSistema/ExemploSistema/frmFuncionarios.cs
@@ -0,0 +1,153 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploSistema
+{
+    public partial class frmFuncionarios : Form
+    {
+        private bool _isNewEmployee;
+        private Employee _selectedEmployee;
+
+        public frmFuncionarios()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFuncionarios_Load(object sender, EventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["sqlserver"].ConnectionString;
+            var listOfEmployess = Enumerable.Empty<Employee>();
+            using (var connection = new SqlConnection(connectionString))
+                listOfEmployess = connection.Query<Employee>("SELECT * FROM EMPLOYEES");
+            
+            dataGrid.DataSource = listOfEmployess;
+            dataGrid.Columns[0].HeaderText = "Id";
+            dataGrid.Columns[1].HeaderText = "Nome";
+            dataGrid.Columns[2].HeaderText = "Estado Civil";
+            dataGrid.Columns[3].HeaderText = "Salário";
+        }
+
+        private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl.SelectedIndex == 1)
+            {
+                _selectedEmployee = new Employee();
+                if (dataGrid.SelectedRows.Count > 0)
+                    _selectedEmployee = dataGrid.SelectedRows[0].DataBoundItem as Employee;
+
+                txtName.Text = _selectedEmployee.Name;
+                cboCivilState.Text = _selectedEmployee.CivilState;
+                txtSalary.Text = _selectedEmployee.Salary.ToString();
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            ((Control)tabList).Enabled = false;
+            tabControl.SelectTab(1);
+
+            txtName.Enabled = true;
+            cboCivilState.Enabled = true;
+            txtSalary.Enabled = true;
+
+            txtName.Text = "";
+            cboCivilState.Text = "";
+            txtSalary.Text = "";
+
+            _selectedEmployee = new Employee();
+            _isNewEmployee = true;
+            
+            btnSave.Visible = true;
+            btnCancel.Visible = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ((Control)tabList).Enabled = true;
+            txtName.Enabled = false;
+            cboCivilState.Enabled = false;
+            txtSalary.Enabled = false;
+
+            _selectedEmployee = new Employee();
+
+            tabControl.SelectTab(0);
+
+            btnSave.Visible = false;
+            btnCancel.Visible = false;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGrid.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("Selecione um funcionário");
+                return;
+            }
+
+            ((Control)tabList).Enabled = false;
+            txtName.Enabled = true;
+            cboCivilState.Enabled = true;
+            txtSalary.Enabled = true;
+
+            _selectedEmployee = dataGrid.SelectedRows[0].DataBoundItem as Employee;
+
+            txtName.Text = _selectedEmployee.Name;
+            cboCivilState.Text = _selectedEmployee.CivilState;
+            txtSalary.Text = _selectedEmployee.Salary.ToString();
+            _isNewEmployee = false;
+
+            tabControl.SelectTab(1);
+
+            btnSave.Visible = true;
+            btnCancel.Visible = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _selectedEmployee.Name = txtName.Text;
+            _selectedEmployee.CivilState = cboCivilState.SelectedItem.ToString();
+            _selectedEmployee.Salary = Convert.ToDecimal(txtSalary.Text);
+
+            var connectionString = ConfigurationManager.ConnectionStrings["sqlserver"].ConnectionString;
+            if (_isNewEmployee)
+                using (var connection = new SqlConnection(connectionString))
+                    connection.Insert<Employee>(_selectedEmployee);
+            else
+                using (var connection = new SqlConnection(connectionString))
+                    connection.Update<Employee>(_selectedEmployee);
+
+            var listOfEmployess = Enumerable.Empty<Employee>();
+            using (var connection = new SqlConnection(connectionString))
+                listOfEmployess = connection.Query<Employee>("SELECT * FROM EMPLOYEES");
+
+            dataGrid.DataSource = listOfEmployess;
+            dataGrid.Columns[0].HeaderText = "Id";
+            dataGrid.Columns[1].HeaderText = "Nome";
+            dataGrid.Columns[2].HeaderText = "Estado Civil";
+            dataGrid.Columns[3].HeaderText = "Salário";
+
+            ((Control)tabList).Enabled = true;
+            txtName.Enabled = false;
+            cboCivilState.Enabled = false;
+            txtSalary.Enabled = false;
+
+            _selectedEmployee = new Employee();
+
+            tabControl.SelectTab(0);
+
+            btnSave.Visible = false;
+            btnCancel.Visible = false;
+        }
+    }
+}
